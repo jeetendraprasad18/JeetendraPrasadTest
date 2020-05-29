f85b53c5eb7dd6efef49aa43f6db7b8a5de83089
jeetendraprasad18 <33753258+jeetendraprasad18@users.noreply.github.com>
2020-05-29 22:19:14 +0530

    Remove Spaces

--- SpellTheNumber/SpellTheNumber/Program.cs
+++ SpellTheNumber/SpellTheNumber/Program.cs
@@ -42,27 +42,17 @@ class SpellTheNumber
 	// Function to print a given number in words 
 	static string convertToWords(long n)
 	{
-
 		// stores word representation of given number n 
 		string out1 = "";
-
 		// handles digits at ten millions and  hundred millions places (if any) 
 		out1 += numToWords((int)(n / 10000000), "crore ");
-
 		// handles digits at hundred thousands and one millions places (if any) 
 		out1 += numToWords((int)((n / 100000) % 100), "lakh ");
-
 		// handles digits at thousands and tens thousands places (if any) 
 		out1 += numToWords((int)((n / 1000) % 100), "thousand ");
-
 		// handles digit at hundreds places (if any) 
 		out1 += numToWords((int)((n / 100) % 10), "hundred ");
-
-		if (n > 100 && n % 100 > 0)
-		{
-			out1 += "and ";
-		}
-
+		if (n > 100 && n % 100 > 0){out1 += "and ";}
 		// handles digits at ones and tens places (if any) 
 		out1 += numToWords((int)(n % 100), "");
 
@@ -72,7 +62,6 @@ class SpellTheNumber
 	static void Main()
 	{
 		Console.WriteLine("Please enter any number :");
-
 		try
 		{       // Read user input
 			var UserInput = Console.ReadLine();
@@ -82,9 +71,7 @@ class SpellTheNumber
 		catch (Exception e)
 		{
 			Console.WriteLine("Not a valid number");
-
 		}
-
 		Console.ReadLine();
 	}
 }
